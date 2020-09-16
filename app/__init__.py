29cfaea7a10020d6356d5d07b4d38b5c55d12c63
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-16 16:04:08 +0300

    my commit add authentication

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 bootstrap = Bootstrap()
@@ -17,6 +22,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Will add the views and forms
 
@@ -27,5 +33,8 @@ def create_app(config_name):
     # setting config
     from .request import configure_request
     configure_request(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
