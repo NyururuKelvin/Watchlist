4f79a8569141bcc6deeb5e76f361beee4f846b4a
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-17 13:13:00 +0300

    my commit add  logins

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='0edb87aa639c75f6778a03ba61b9eba7'
 export SECRET_KEY='Flask WTF Secret Key'
+export MAIL_USERNAME=nyururukelvin99@gmail.com
+export MAIL_PASSWORD=nyururu
 
 python3.6 manage.py server
